19c0640488b9c5e761ad7768d56c2568bf0fe57a
Marie Peron <marie.prn77@gmail.com>
2026-01-31 21:58:28 +0100

    Création faq

---
+++ src/components/faq.jsx
@@ -0,0 +1,11 @@
+import "../styles/faq.scss";
+
+const FAQ = () => 
+  <div className="faq">
+    <h2>F.A.Q</h2>
+    <div className="faqContenu">
+        
+    </div>
+  </div>;
+
+export default FAQ;
